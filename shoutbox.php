8cc7cb516de662af8ac1566b7afa345af4fb34b6
funkydylan <dylan.weyns@live.be>
2023-03-21 11:24:04 +0100

    push

--- shoutbox.php
+++ shoutbox.php
@@ -3,8 +3,10 @@ include 'includes/header.php';
 include 'database/connectie.php'
 ?>
 <?php
-    $query = "SELECT * FROM shoutbox";
-    $berichten = mysqli_query($con,$query);
+
+$query = "SELECT * FROM shoutbox";
+$berichten = mysqli_query($con, $query);
+
 ?>
     <main>
         <div class="uk-card uk-card-default uk-width-1-2@m uk-align-center">
@@ -17,32 +19,44 @@ include 'database/connectie.php'
             </div>
             <div class="uk-card-body">
                 <ul class="uk-list">
-                    <?php while ($rij = mysqli_fetch_assoc($berichten)) :
-                    ?>
+                    <?php
+                    while($rij = mysqli_fetch_assoc($berichten)):
+                        ?>
                         <li>
-                            <span class="uk-text-meta uk-margin-remove-top"><?php echo $rij['tijd']?> - </span>
-                            <span class="uk-text-bold"><?php echo $rij['gebruiker']?>: </span>
-                            <span class="uk-text-normal"><?php echo $rij["bericht"] ?></span>
+                            <span class="uk-text-meta uk-margin-remove-top"><?php echo $rij['tijd'] ?> - </span>
+                            <span class="uk-text-bold"><?php echo $rij['gebruiker'] ?>: </span>
+                            <span class=".uk-text-normaal"><?php echo $rij['bericht'] ?></span>
                         </li>
-                    <?php endwhile; ?>
+                    <?php endwhile;?>
                 </ul>
             </div>
             <div class="uk-card-footer uk-card-primary">
+                <?php
+                if(isset($_GET['fout'])): ?>
+                    <div class="uk-text-warning uk-text-right uk-animation-fade">
+                        <?php echo $_GET['fout']; ?>
+                    </div>
+                <?php endif; ?>
+
+
                 <form method="post" action="shoutbox/process.php">
                     <div class="uk-margin">
                         <div class="uk-inline">
                             <span class="uk-form-icon" uk-icon="icon: user"></span>
                             <input class="uk-input" type="text" placeholder="Voornaam" name="gebruiker" aria-label="Not clickable icon">
                         </div>
-                        <div class="uk-margin">
-                            <div class="uk-inline">
-                                <span class="uk-form-icon" uk-icon="icon: file-edit"></span>
-                                <input class="uk-input uk-form-width-large" type="text" placeholder="Bericht" name="bericht" aria-label="Not clickable icon">
-                            </div>
                     </div>
-                        <div>
-                            <input type="submit" class="uk-button uk-button-danger" name="verzenden" value="SCHREEUW HET UIT!!" aria-label="Not clickable icon" >
+                    <div class="uk-margin">
+                        <div class="uk-inline">
+                            <span class="uk-form-icon" uk-icon="icon: file-edit"></span>
+                            <input class="uk-input uk-form-width-large" type="text" placeholder="Bericht" name="bericht" aria-label="Not clickable icon">
                         </div>
+                    </div>
+                    <div class="uk-margin">
+                        <div class="uk-inline">
+                            <input type="submit" name="verzenden" class="uk-button uk-button-danger" value="SCHREEUW HET UIT!!" aria-label="Not clickable icon">
+                        </div>
+                    </div>
                 </form>
             </div>
         </div>
--- shoutbox/process.php
+++ shoutbox/process.php
@@ -1,9 +1,25 @@
 <?php
+
 include '../database/connectie.php';
 
-    if(isset($_POST['verzenden'])){
-        $gebruiker = mysqli_real_escape_string($con, $_POST['gebruiker']);
-        $bericht = mysqli_real_escape_string($con, $_POST['bericht']);
+if(isset($_POST['verzenden'])){
+    $gebruiker = mysqli_real_escape_string($con, $_POST['gebruiker']);
+    $bericht = mysqli_real_escape_string($con, $_POST['bericht']);
 
-        
+
+    if (!isset($gebruiker) || $gebruiker == '' || !isset($bericht) || $bericht == '') {
+        $error = "Vul alle velden in!";
+        header("Location: ../shoutbox.php?fout=".urlencode($error));
+        exit();
+    } else {
+        $query = "INSERT INTO shoutbox (gebruiker, bericht) VALUES ('$gebruiker', '$bericht')";
+
+        if (!mysqli_query($con, $query)) {
+            die('Fout: ' . mysqli_error($con));
+        } else {
+            header("Location: ../shoutbox.php");
+            exit();
+        }
     }
+}
+?>
